49fa3cc22e14d30561b6de057180a2b7bde62075
Aliaqa Alizada <alimhd276@gmail.com>
2026-05-29 15:17:09 +0200

    adde new SVG icons and update dialog functionality and styles for better user experience and visual appeal. Added new SVG icons for outward arrow and next arrow. Updated the dialog styles to include animations for opening and closing, as well as a backdrop fade effect. Improved the hover effects on buttons and icons for better interactivity. Also added functionality to close the dialog when clicking outside of it and to navigate to the next dialog seamlessly.

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-function openDialog(dialogId){
-    document.getElementById(dialogId).showModal();
+
+function openDialog(dialogId) {
+  const dialog = document.getElementById(dialogId);
+  dialog.showModal();
+  document.body.style.overflow = "hidden";
 }
 
-function closeDialog(dialogId){
-    document.getElementById(dialogId).close();
+function closeDialog(dialogId) {
+  const dialog = document.getElementById(dialogId);
+  dialog.close();
+  document.body.style.overflow = "auto";
 }
+
+const dialogs = Array.from(document.querySelectorAll('.project-dialog'));
+
+dialogs.forEach(dialog => {
+    dialog.addEventListener('click', (e) => {
+
+            const rect = dialog.getBoundingClientRect();
+
+            const clickedInside = 
+            e.clientX >= rect.left &&
+            e.clientX <= rect.right &&
+            e.clientY >= rect.top &&
+            e.clientY <= rect.bottom;
+
+            if(!clickedInside) {
+                dialog.close();
+            }
+            document.body.style.overflow = 'auto'; 
+    });
+});
+
+dialogs.forEach((dialog, index) => {
+    const nextBtn = dialog.querySelector(".next-project");
+    nextBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        const currentDialog = dialogs[index];
+        const nextDialog = dialogs[(index + 1) % dialogs.length];
+        currentDialog.classList.add("closing");
+
+        setTimeout(() => {
+            currentDialog.close();
+            currentDialog.classList.remove("closing");
+                requestAnimationFrame(() => {
+                nextDialog.showModal();
+                document.body.style.overflow = "hidden";
+            });
+        }, 250);
+    });
+});
