13648e1a7b8127d2f8f56fd1c509581385f16755
Aliaqa Alizada <alimhd276@gmail.com>
2026-06-15 01:33:21 +0200

    add "Remove unused image files" and "Hide hero title and back arrow on legal notice and privacy policy pages"

--- script.js
+++ script.js
@@ -393,8 +393,9 @@ const observer = new IntersectionObserver(entries => {
 
         if(entry.isIntersecting){
             entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
+
+            // danach nicht mehr beobachten
+            observer.unobserve(entry.target);
         }
 
     });
@@ -402,5 +403,6 @@ const observer = new IntersectionObserver(entries => {
 }, {
     threshold: 0.2
 });
+
 
 hiddenElements.forEach(el => observer.observe(el));
